133ca1179128dfc7dac96d26dab83087173e6150
allenwalker2015 <00053812@uca.edu.sv>
2021-01-18 10:41:32 -0600

    Se ha corregido las contraseñas en los dummy users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
             [
                 'username' => 'tempuser1',
                 'email' => 'tempuser1@gmail.com',
-                'password' => 'password',
+                'password' => bcrypt('password'),
                 'name' => 'Temp User 1',
                 'birthday' => '1997/12/10',
                 'phone' => '78716568'
@@ -24,7 +24,7 @@ class UserSeeder extends Seeder
         User::create([
             'username' => 'tempuser2',
             'email' => 'tempuser2@gmail.com',
-            'password' => 'password',
+            'password' => bcrypt('password'),
             'name' => 'Temp User 2',
             'birthday' => '1998/12/10',
             'phone' => '78716567'
@@ -32,7 +32,7 @@ class UserSeeder extends Seeder
         User::create([
                 'username' => 'tempuser3',
                 'email' => 'tempuser3@gmail.com',
-                'password' => 'password',
+                'password' => bcrypt('password'),
                 'name' => 'Temp User 3',
                 'birthday' => '1999/12/10',
                 'phone' => '78716566'
